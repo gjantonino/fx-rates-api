39dd4d46c692f2528f3aa795ca2d33d851c655c8
Gregorio Antonino <gjantonino@gmail.com>
2021-03-22 10:35:33 -0300

    Updates to routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const FxRateController = require('./controllers/fxRate/fxRate.js')
+const FxRateController = require('./controllers/fxRate/fxRate.js');
+const services = require('./services/index.js');
 
 module.exports = {
     name: 'routes',
     register: async (server, options) => {
         server.route([
+            {
+                method: 'GET',
+                path: '/',
+                handler: services.healthcheck
+            },
             {
                 method: 'GET',
                 path: '/rates',
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const fixerio = require('./fixerio-api/index.js');
 const config  = require('./config.js');
+const healthcheck = require('./healthcheck.js');
 
 module.exports = {
     fixerio,
-    config
+    config,
+    healthcheck
 }
