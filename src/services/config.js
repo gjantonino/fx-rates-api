40b24ff37170aded6420bd860419ed9a78dac8e8
Gregorio Antonino <gjantonino@gmail.com>
2021-03-22 15:49:44 -0300

    removed access_key from repo

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     fixerio: {
         url: 'http://data.fixer.io/api',
-        access_key: 'ffcc344a3f31700c0020d166fd17ea96',
+        access_key: process.env.FIXERIO_ACCESS_KEY || require('../../local/fixerio.js').FIXERIO_ACCESS_KEY,
         defaultBaseSymbol: 'EUR',
         allowedPairs: [
             'EURUSD',
